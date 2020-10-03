1ed664124c7f7b2d7dac5d0ff9ce23cb5f4285b3
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-10-02 19:33:16 -0600

    Explaining first commit for setting up node server and second commit
    Cloning and Setup
    The first thing you want to do when cloning a repo or starting a project within our local environment is install all dependencies within our package.json file. npm i in terminal
    https://github.com/udacity/cloud-developer/tree/master/course-02/exercises/udacity-c2-restapi
    https://www.youtube.com/watch?v=oetVXXhUTYo
    
    throw new Error('our error message');
    
    - On an empty folder, we first initialize anew node app and create the package.json file by 'npm init -y' The -y flag makes us not yo answer any questions
    - install express package by 'npm i express --save' , the framework will be installed and gets added to our package.json automatically
    - install typescript  package as a dev dependency 'npm i typescript --save-dev'
    - added a script command 'tsc' to our scripts section in our package.json file so that we can run the tsc command to transpile our ts files to js
    
    - create a tconfig.json file to help tell the compiler how to compile our ts file(s) by 'npm run tsc -- --init' Make sure you have the tsc command in your script at package.json and save the file before you initialize/create the config file
    - inside the tconfig file, we look for the 'outDir', uncomment it and set the files that we will transpile to be located in the 'www' folder
    - we look for baseUrl in tconfig file and uncomment it out
    - in the tconfig file also we uncomment sourcemap and set it to true. This is because we will have for evey ts file it creates also a map file . This will help us debug ts file directly in the browser.
    - we also uncomment moduleResolution in the tconfig file
    - Added the include key, and set only the file(s) to be compiled by the tsc to be only the ones located on the src folder
    - uncommented the path and added the node_modules
    - uncommentd also noUnusedLocals, noUnusedParameters, noImplicitReturns to help me write a more clean code and avoid syntax or runtime error
    -FYI: i left the 'target' to es5, just because this project involved some html code, that i will run in browser, so i reduced the targeted js version that we want our ts file(s) to be transpile to when running the js in browser. The more lastest version you use like es6, the faster, your js file will run. This will be ideal if we are just running pure backend code to be delpoyed in our server computer in cloud or local.
    
    Since typescript does not know express classes, we need to install a specific package for ts to recognize express type class. The package is called @types/express. To install the package we use 'npm install @types/express --save'
    
    - create our main entry-point file called server.ts
    
    Note
    - When installing dependencies, the --save, will safe the package in the production dependencies and --save-dev will save the dependency in the devdependencies
    - The package listed in devdependecies means those will be used for development period of the node server, while the ones with just --save flag will be used for production.
    - If you dont use the flag, by default will be saved and installed for in production
    
    FOR THIS SECOND COMMIT
    - i edited my tconfig file, commented the noUnusedParameters just because the express get method for our root url has an used parameter. i wanted to overlook that. i probably will leave it like that
    - I made some minor changes to my package.json file. mainly was adding a script command to start the node server locally
    - wrote a basic node server to print out hello world at the root url

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-//const express = require()
+import express from 'express';
+//const express = require('express'); //you  can inport or require the file. Import is a more best practise
+
+const app = express();
+const port = process.env.PORT || 8080; // we are saying if there is a port environment vairbale it will use that, otherwise we initialize the default port to listen to manually
+
+// Root URI call
+app.get( "/", async ( req, res ) => {
+    res.status(200).send( "Hello World" );
+  } );
+
+// Start the Server
+app.listen( port, () => { //we pass the port variable as first parameter and a callback as second parameter
+    console.log( `server running http://localhost:${ port }` );
+    console.log( `press CTRL+C to stop server` );
+} );
